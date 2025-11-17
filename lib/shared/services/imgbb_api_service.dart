0daa0545f8a7f69f3d7b6d8e6331e48ac0013446
XaerickSReborn <u202213358@upc.edu.pe>
2025-11-17 18:03:35 -0500

    feat(imgbb_api_service): add support for uploading image bytes
    Add new method to upload images from byte data along with existing file path upload. This provides more flexibility when working with in-memory image data.

--- lib/shared/services/imgbb_api_service.dart
+++ lib/shared/services/imgbb_api_service.dart
@@ -1,16 +1,28 @@
 import 'package:dio/dio.dart';
 import '../../config/dio_client.dart';
 import '../../environments/environment.dart';
+import 'dart:typed_data';
 
 class ImgbbApiService {
   final Dio _dio = DioClient.instance.dio;
+  final Dio _dioNoAuth = Dio();
 
   Future<Map<String, dynamic>> uploadImage(String filePath) async {
     final formData = FormData.fromMap({
       'image': await MultipartFile.fromFile(filePath),
     });
     final url = 'https://api.imgbb.com/1/upload?key=${Environment.imgbbApiKey}';
-    final res = await _dio.post(url, data: formData);
+    final res = await _dioNoAuth.post(url, data: formData);
+    final data = Map<String, dynamic>.from(res.data);
+    return Map<String, dynamic>.from(data['data']);
+  }
+
+  Future<Map<String, dynamic>> uploadImageBytes(Uint8List bytes, String filename) async {
+    final formData = FormData.fromMap({
+      'image': MultipartFile.fromBytes(bytes, filename: filename),
+    });
+    final url = 'https://api.imgbb.com/1/upload?key=${Environment.imgbbApiKey}';
+    final res = await _dioNoAuth.post(url, data: formData);
     final data = Map<String, dynamic>.from(res.data);
     return Map<String, dynamic>.from(data['data']);
   }
